1a5663c6f9236ef023f4db5dfcaab1d844eb035c
Germán Darío <Germn Daro@Mela-VAIO>
2016-08-11 11:50:59 -0500

    Revert "Entity User."
    This reverts commit 35bcd927a41746721c6b70409d1c2d80210a15c2.

--- src/GDCP/UserBundle/Controller/UserController.php
+++ src/GDCP/UserBundle/Controller/UserController.php
@@ -9,29 +9,6 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-       $db = $this->getDoctrine()->getManager();
-       
-       $user = $db->getRepository('GDCPUserBundle:User')->findAll();
-       
-       $res  = 'Lista de usuarios: <br />';
-       
-       foreach($user as $user)
-       {
-           
-        $res .= 'Usuario: ' . $user->getUserName() . ' - Email: ' . $user->getEmail() . '<br />';       
-       
-       }
-       
-       return new Response($res);
-    }
-    
-    public function viewAction($id)
-    {
-        
-        $repository = $this->getDoctrine()->getRepository("GDCPUserBundle:User");
-        
-        $user = $repository->find($id);
-        
-        return new Response('Usuario: ' . $user->getUserName(). ' con email: ' . $user->getEmail());
+        return new Response('Bienvenido a mi modulo de usuarios');
     }
 }
